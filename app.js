d1511bf8172ab73114f9c4e2f0331cb697e168c4
Greg Forster <12greg21@gmail.com>
2020-11-13 14:49:58 -0500

    Majority of HTML done

---
+++ app.js
@@ -0,0 +1,85 @@
+const solarApp = {};
+
+const planets = {
+  mercury: {
+
+  },
+  venus: {
+
+  },
+  earth: {
+    name: "Earth",
+    description: "Welcome to Earth! Original home to humans.",
+    temp: 3,
+    gravity: 1,
+    travelTime: 0
+  },
+  mars: {
+    name: "Mars",
+    description: "Undergoing terraforming currently.",
+    temp: 3,
+    gravity: 0.38,
+    travelTime: 0.72
+  },
+  jupiter: {
+    name: "",
+    description: "",
+    temp: 0,
+    gravity: ,
+    travelTime: 0
+  },
+  saturn: {
+    name: "Saturn",
+    description: "",
+    temp: 1,
+    gravity: ,
+    travelTime: 0
+  },
+  uranus: {
+    name: "Uranus",
+    description: "",
+    temp: 1,
+    gravity: ,
+    travelTime: 0
+  },
+  neptune: {
+    name: "",
+    description: "",
+    temp: 2,
+    gravity: ,
+    travelTime: 0
+  },
+  pluto: {
+    name: "",
+    description: "",
+    temp: 1,
+    gravity: ,
+    travelTime: 0
+  },
+  europa: {
+    name: "",
+    description: "",
+    temp: 2,
+    gravity: ,
+    travelTime: 0
+  },
+  titan: {
+    name: "Titan",
+    description: "Titan is the largest moon of Saturn. Titan is the only moon in the solar system to have a dense atmosphere. It is also the only known celestial body, aside from Earth, to have evidence of stable bodies of surface liquid."
+  },
+  ganymede: {
+    name: "Ganymede",
+    description: "Ganymede is the largest and most massive of the solar system's moons. It is a moon of Jupiter. It is also the only moon within the solar system to have a magnetosphere.",
+    temp: 1,
+    gravity: 1.428,
+    travelTime: 0.6283
+  },
+  enceladus: {
+    name: "Enceladus",
+    description: "Enceladus is the sixth-largest moon of Saturn. It is about 500 kilometers in diameter. Ever since Enceladus was seen shooting plumes of water vapor and ice from cracks in it's frozen crust more than a decade ago scientists have been pondering the possibility of life deep within it's oceans.",
+    temp: 1,
+    gravity: 0.113,
+    travelTime: 1.272,
+    source: "https://www.latimes.com/science/sciencenow/la-sci-sn-cassini-saturn-enceladus-20170413-story.html"
+  }
+}
