a887b4eed051d9ef54e9298189ae92921986750b
Greg Forster <12greg21@gmail.com>
2020-11-13 18:28:25 -0500

    Planets object created

--- app.js
+++ app.js
@@ -1,75 +1,88 @@
 const solarApp = {};
 
 const planets = {
-  mercury: {
-
+  mercury: {    
+    name: "Mercury",
+    description: "Mercury is the smallest and innermost planet of the solar system. The temperature on the surface of Mercury can reach as hot as 425 C during the daytime and as low as -173 C at night.",
+    temp: 4,
+    gravity: 3.7,
+    travelTime: 0.077
   },
   venus: {
-
+    name: "Venus",
+    description: "Venus is the second planet from the Sun. Cities floating on Venus's clouds is currently undergoing R&D.",
+    temp: 5,
+    travelTime: 0.042
   },
   earth: {
     name: "Earth",
     description: "Welcome to Earth! Original home to humans.",
-    temp: 3,
+    temp: 4,
     gravity: 1,
     travelTime: 0
   },
   mars: {
     name: "Mars",
-    description: "Undergoing terraforming currently.",
+    description: "Mars is now the second home to humans and is currently being terraformed.",
     temp: 3,
     gravity: 0.38,
-    travelTime: 0.72
+    travelTime: 0.78340
   },
   jupiter: {
-    name: "",
-    description: "",
-    temp: 0,
-    gravity: ,
-    travelTime: 0
+    name: "Jupiter",
+    description: "Jupiter is an absolute unit, being the largest in the solar system. It has a mass of one-thousandth (1/1000) that of the sun, but two-and-a-half times that of all the other planets in the solar system combined.",
+    temp: 2,
+    gravity: 24.79,
+    travelTime: 0.628730,
+    source: "https://en.wikipedia.org/wiki/Jupiter"
   },
   saturn: {
     name: "Saturn",
-    description: "",
+    description: "Saturn is a gas giant and is the second largest planent in the solar system, following Jupiter. Saturn has an average density of only one-eighth the average density of Earth.",
     temp: 1,
-    gravity: ,
-    travelTime: 0
+    gravity: 10.44,
+    travelTime: 1.275
   },
   uranus: {
     name: "Uranus",
-    description: "",
+    description: "Uranus is the third largest planet of the solar system. It is often referred to as an 'ice giant' planet. Uranus hits the coldest temperatures of any planet.",
     temp: 1,
-    gravity: ,
-    travelTime: 0
+    gravity: 8.87,
+    travelTime: 2.723950,
+    source: "https://space-facts.com/uranus/"
   },
   neptune: {
-    name: "",
-    description: "",
-    temp: 2,
-    gravity: ,
-    travelTime: 0
+    name: "Neptune",
+    description: "Neptune is the last planet within the solar system. It is 17 times the mass of Earth and is the densest giant planet.",
+    temp: 1,
+    gravity: 11.15,
+    travelTime: 4.3514
   },
   pluto: {
-    name: "",
-    description: "",
+    name: "Pluto",
+    description: "Pluto is a dwarf planet found within the Kuiper belt, a ring of bodies beyond the orbit of Neptune. Pluto was the first and largest Kuiper belt object to be discovered in 1930. In 2006 it was demoted to the life of a dwarf planet.",
     temp: 1,
-    gravity: ,
-    travelTime: 0
+    gravity: 0.62,
+    travelTime: 5.1794,
+    source: "https://solarsystem.nasa.gov/planets/dwarf-planets/pluto/in-depth/"
   },
   europa: {
-    name: "",
-    description: "",
+    name: "Europa",
+    description: "Europa is a moon in orbit of Jupiter. It is the sixth-closest to the planet out of all the 79 moons of Jupiter. Europa has a possibility of harboring life in its under-ice ocean, perhaps in an evironment closely resembling Earth's deep-ocean hydrothermal vents.",
     temp: 2,
-    gravity: ,
-    travelTime: 0
+    gravity: 1.315,
+    travelTime: 0.6283
   },
   titan: {
     name: "Titan",
-    description: "Titan is the largest moon of Saturn. Titan is the only moon in the solar system to have a dense atmosphere. It is also the only known celestial body, aside from Earth, to have evidence of stable bodies of surface liquid."
+    description: "Titan is the largest moon of Saturn. Titan is the only moon in the solar system to have a dense atmosphere. It is also the only known celestial body, aside from Earth, to have evidence of stable bodies of surface liquid.",
+    temp: 1,
+    gravity: 1.352,
+    travelTime: 1.5485
   },
   ganymede: {
     name: "Ganymede",
-    description: "Ganymede is the largest and most massive of the solar system's moons. It is a moon of Jupiter. It is also the only moon within the solar system to have a magnetosphere.",
+    description: "Ganymede is a moon of Jupiter. It is the largest and most massive of the solar system's moons. It is also the only moon within the solar system to have a magnetosphere.",
     temp: 1,
     gravity: 1.428,
     travelTime: 0.6283
@@ -81,5 +94,13 @@ const planets = {
     gravity: 0.113,
     travelTime: 1.272,
     source: "https://www.latimes.com/science/sciencenow/la-sci-sn-cassini-saturn-enceladus-20170413-story.html"
+  },
+  io: {
+    name: "Io",
+    description: "Io is the third-largest of the four Galilean moons of planet Jupiter. It has the highest density for a moon in the solar system, yet is only the fourth-largest.",
+    temp: 3,
+    gravity: 1.796,
+    travelTime: 24.79
   }
 }
+
